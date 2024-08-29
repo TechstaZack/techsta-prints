0b790a3bb84a3152d4a2c73c74d439ee2e07716d
TechstaZack <127003749+TechstaZack@users.noreply.github.com>
2024-08-29 21:14:40 +1000

    Update index.js

--- index.js
+++ index.js
@@ -285,6 +285,44 @@ function sharkUpdated(){
       document.getElementById('dynamic-price').innerHTML = price;
   })
 }
+function catUpdated(){
+  console.log('ran')
+  const value1 = document.getElementById('option1').value;
+  const value3 = document.getElementById('option3').value;
+  document.getElementById('dynamic-price').innerHTML = 'Loading Price...'
+  document.getElementById('outostock').innerHTML = ''
+  for (let i = 0; i <= outOfStock.length; i++){
+    if(value1 == outOfStock[i]){
+      document.getElementById('dynamic-price').innerHTML = ''
+      document.getElementById('outostock').innerHTML = 'Out of stock'
+      return
+    }
+  }
+  fetch(`https://kilvington-prints-api-ahf6ft7xlq-uc.a.run.app/priceid?lookup=cat-${value1}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+      let price = data.aud;
+      price = price * value3
+      if(price < 100){
+          price = `¢${price}`
+      }else{
+          price = price / 100
+          price = `$${price}`
+          if(price.includes('.') && !price.endsWith('0') && price.length < 5) {
+              price = `${price}0`
+          }else if(price.length < 5){
+              price = `${price}.00`
+          }
+
+      }
+      document.getElementById('dynamic-price').innerHTML = price;
+  })
+}
 
 
 //add to cart
@@ -718,6 +756,77 @@ function sharkaddtocart(){
   }
   $('#modal').modal('hide')
 }
+function cataddtocart(){
+  const value1 = document.getElementById('option1').value;
+  const value3 = document.getElementById('option3').value;
+  const cartprices = localStorage.getItem('cart-price')
+  const quantities = localStorage.getItem('cart-quantity')
+  const amount = localStorage.getItem('cart-amount')
+  
+  if(cartprices == null || cartprices == ''){
+      fetch(`https://kilvington-prints-api-ahf6ft7xlq-uc.a.run.app/priceid?lookup=cat-${value1}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+      localStorage.setItem('cart-price', data.priceid)
+      localStorage.setItem('cart-quantity', value3)
+      localStorage.setItem('cart-amount', '1')
+      document.getElementById('cart').innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+            </svg> 1`
+  })
+  }else{
+      fetch(`https://kilvington-prints-api-ahf6ft7xlq-uc.a.run.app/priceid?lookup=cat-${value1}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+      let alreadyin = false
+      let alreadyinno = null
+      let priceid = data.priceid;
+      console.log(priceid)
+      const priceArray = cartprices.split(',');
+      const length = priceArray.length - 1;
+        for (let i = 0; i <= length; i++){
+          if(priceArray[i].includes(priceid)){
+              alreadyin = true
+              alreadyinno = i
+          }
+      if(alreadyin == true){
+          const quantityArray = quantities.split(',')
+          quantityArray[alreadyinno] = value3
+          let updatedquantities = null
+          updatedquantities = quantityArray[0]
+          if(quantityArray[1] != undefined){
+          for (let i = 1; i <= quantityArray.length; i++){
+              let toadd = quantityArray[i]
+              updatedquantities = `${updatedquantities},${toadd}`
+              i++
+          }}
+          localStorage.setItem('cart-quantity',updatedquantities)
+      }else{
+          const newPriceList = `${cartprices},${data.priceid}`
+          const newQuantitiesList = `${quantities},${value3}`
+          const newAmount = parseInt(amount) + 1
+          localStorage.setItem('cart-price', newPriceList)
+          localStorage.setItem('cart-quantity', newQuantitiesList)
+          document.getElementById('cart').innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+            </svg> ${newAmount}`
+          localStorage.setItem('cart-amount',newAmount)
+      }
+      }
+  })
+  }
+  $('#modal').modal('hide')
+}
 
 //modals
 
@@ -1080,7 +1189,7 @@ function sharkModal(){
         <hr>
         <img src="images/shark.jpg" width="225px", height="300px">
         <br><br>
-        <h5 style='color: grey;' id="dynamic-price">$4</h5>
+        <h5 style='color: grey;' id="dynamic-price">$6.50</h5>
                   <h3 style='color: red !important;' id="outostock"></h3>
         <hr>
 
@@ -1117,6 +1226,64 @@ function sharkModal(){
       </div>
       <div class="modal-footer" style='object-position: center;'>
           <button data-dismiss="modal" class="btn btn-outline-secondary" id="addtocart" onclick="sharkaddtocart()">Add To Cart <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+              <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z"/>
+              <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+            </svg></button>
+      </div>
+    </div>
+  </div>`
+}
+function catModal(){
+  document.getElementById('productModal').innerHTML = `<div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="productModalLabel">Product Details</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h1 style='color:grey;'>Cat</h1>
+        <hr>
+        <img src="images/cat.jpg" width="225px", height="300px">
+        <br><br>
+        <h5 style='color: grey;' id="dynamic-price">$3</h5>
+                  <h3 style='color: red !important;' id="outostock"></h3>
+        <hr>
+
+        <h5>Colour:</h5>
+
+        <select name="outercolour" id="option1" class="btn btn-outline-secondary" onchange="catUpdated()">
+          <option value="bla">Black</option>
+          <option value="sk">Sky Blue</option>
+          <option value="br">Pink</option>
+          <option value="go">Gold</option>
+          <option value="or">Orange</option>
+          <option value="gr">Green</option>
+          <option value="bl">Blue</option>
+          <option value="si">Silver</option>
+          <option value="wh">White</option>
+          <option value="re">Red</option>
+        </select>
+        <h5>Quantity</h5>
+
+        <select name="outercolour" id="option3" class="btn btn-outline-secondary" onchange="catUpdated()">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+        <hr>
+        <h5 class="credit">Fulfilled by Zac Cooper • <a href='https://www.printables.com/model/965961-the-cat' target="_blank">3D model download Here</a></h5>
+      </div>
+      <div class="modal-footer" style='object-position: center;'>
+          <button data-dismiss="modal" class="btn btn-outline-secondary" id="addtocart" onclick="cataddtocart()">Add To Cart <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
               <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9z"/>
               <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
             </svg></button>
